c6df561ef3a297883cc3c9357af08c69058b1625
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-09 21:22:42 -0500

    scaffolded hotelRoom respository and interface

---
+++ Data/Interfaces/IHotelRoomRepository.cs
@@ -0,0 +1,22 @@
+﻿using AsyncInn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Data.Interfaces
+{
+    public interface IHotelRoomRepository
+
+    {
+        Task<IEnumerable<Hotel>> GetHotelRooms();
+
+        Task<Hotel> GetHotelRoom(int id);
+
+        Task<bool> DeleteHotelRoom(int id);
+
+        Task PostHotelRoom(Hotel hotel);
+
+        Task<bool> PutHotelRoom(Hotel hotel);
+    }
+}
