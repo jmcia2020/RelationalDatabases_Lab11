41dd2318e624bf34809f22ece4073ba5f02af19e
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-12 14:51:07 -0500

    Added Room Ameinity Model, Ids and navigation properties

---
+++ Models/RoomAmenity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AsyncInn.Models
+{
+    public class RoomAmenity
+    {
+        public int RoomId { get; set; }
+        public int AmenityId { get; set; }
+
+        //Navigation Properties
+
+        public Room Room { get; set; }
+
+        public Amenity Amenitiy { get; set; }
+    }
+}
