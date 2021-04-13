7168ec758490a540423f27cfbe7bf3f439b4fbe1
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-12 22:49:13 -0500

    fixed routes for hotelRooms

--- Controllers/HotelRoomController.cs
+++ Controllers/HotelRoomController.cs
@@ -8,19 +8,19 @@ using System.Threading.Tasks;
 
 namespace AsyncInn.Controllers
 {
-    [Route("api/Hotels/{hotelId}/Rooms")]
+    [Route("/api/Hotels/{hotelId}/Rooms")]
     [ApiController]
     public class HotelRoomController : ControllerBase
     {
         // GET: api/<HotelRoomController> 
-        [HttpGet("api/Hotels/{hotelId}/Rooms")]  // /api/Hotels/{hotelId}/Rooms
+        [HttpGet]  // /api/Hotels/{hotelId}/Rooms
         public IEnumerable<string> Get(int hotelId)
         {
             return new string[] { "hotelId", "roomNumber" };
         }
 
         // GET api/<HotelRoomController>/5 
-        [HttpGet("{id}")] // /api/Hotels/{hotelId}/Rooms/{roomNumber}
+        [HttpGet("{roomNumber}")] // /api/Hotels/{hotelId}/Rooms/{roomNumber}
         public string Get(int hotelId, int roomNumber)
         {
             return "value";
@@ -33,13 +33,13 @@ namespace AsyncInn.Controllers
         }
 
         // PUT api/<HotelRoomController>/5
-        [HttpPut("{id}")] // /api/Hotels/{hotelId}/Rooms/{roomnumber}
+        [HttpPut("{roomNumber}")] // /api/Hotels/{hotelId}/Rooms/{roomnumber}
         public void Put(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/<HotelRoomController>/5
-        [HttpDelete("{id}")] // /api/Hotels/{hotelId}/Rooms/{roomnumber}
+        [HttpDelete("{roomNumber}")] // /api/Hotels/{hotelId}/Rooms/{roomnumber}
         public void Delete(int id)
         {
         }
