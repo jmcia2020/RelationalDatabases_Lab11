0a1ac7760b48effd984dd98aa74483e79fe68cc2
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-15 22:54:24 -0500

    Add JwtTokenSErvice dependencies

--- Services/JwtToken Services.cs
+++ Services/JwtToken Services.cs
@@ -1,4 +1,6 @@
 ﻿using AsyncInn.Models.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +10,15 @@ namespace AsyncInn.Services
 {
     public class JwtTokenService
     {
+        private readonly IConfiguration configuration;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public JwtTokenService(IConfiguration configuration, SignInManager<ApplicationUser> signInManager)
+        {
+            this.configuration = configuration;
+            this.signInManager = signInManager;
+        }
+        
         public async Task<string> GetToken(ApplicationUser user, TimeSpan expiresIn)
         {
             return "token!";
