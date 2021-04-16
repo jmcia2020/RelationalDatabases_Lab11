1da54e3987e8972cf9120dcf06e6eaa26cc796ae
jmcia2020 <jeanmmcmahon@gmail.com>
2021-04-15 23:10:19 -0500

    Create our first JSON Web Token (JWT)

--- Services/JwtToken Services.cs
+++ Services/JwtToken Services.cs
@@ -1,9 +1,12 @@
 ﻿using AsyncInn.Models.Identity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace AsyncInn.Services
@@ -21,7 +24,28 @@ namespace AsyncInn.Services
         
         public async Task<string> GetToken(ApplicationUser user, TimeSpan expiresIn)
         {
-            return "token!";
+            var principal = await signInManager.CreateUserPrincipalAsync(user);
+            if (principal == null)
+                return null;
+
+            var signingKey = GetSecurityKey(configuration);
+
+            var token = new JwtSecurityToken(
+                expires: DateTime.UtcNow + expiresIn,
+                signingCredentials: new SigningCredentials(signingKey, SecurityAlgorithms.HmacSha256),
+                claims: principal.Claims
+                );
+
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+
+        private static SecurityKey GetSecurityKey(IConfiguration configuration)
+        {
+            var secret = configuration["JWT:Secret"];
+            if (secret == null) throw new InvalidOperationException("JWT:Secret is missing!");
+
+            var secretBytes = Encoding.UTF8.GetBytes(secret);
+            return new SymmetricSecurityKey(secretBytes);
         }
     }
 }
